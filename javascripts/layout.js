9967a0aa71308159c57475fb9f4a15ec57aa34d0
Travis CI (Shuma Yoshioka) <s64.stdio+gh-seisahachioji@gmail.com>
2014-07-01 00:00:07 +0000

    Update

--- javascripts/layout.js
+++ javascripts/layout.js
@@ -1,4 +1,29 @@
 (function() {
-  $(function() {});
+  var mainPosition, onscroll;
+
+  mainPosition = null;
+
+  onscroll = function() {
+    var body, header, headerHeight, position;
+    position = $(window).scrollTop();
+    body = $('body').eq(0);
+    header = $('#nav');
+    $(body).attr('data-scroll-top', position);
+    headerHeight = $(header).height();
+    if ((mainPosition != null) && (mainPosition - headerHeight) <= position) {
+      return $(body).attr('data-scrolled-main', 'data-scrolled-main');
+    } else {
+      return $(body).removeAttr('data-scrolled-main');
+    }
+  };
+
+  $(window).on('scroll', onscroll);
+
+  $(function() {
+    return (function() {
+      mainPosition = $('#mainborder').offset().top;
+      return onscroll();
+    })();
+  });
 
 }).call(this);
